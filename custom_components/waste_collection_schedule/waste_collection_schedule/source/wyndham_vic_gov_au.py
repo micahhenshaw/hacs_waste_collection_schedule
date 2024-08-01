d1676bafbb971d1307ddce9d2da29db6a5111565
Micah Henshaw <31399816+micahhenshaw@users.noreply.github.com>
2024-08-01 12:54:36 +1000

    Update wyndham_vic_gov_au.py
    Temp fix as the desktop version of their site is down

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/wyndham_vic_gov_au.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/wyndham_vic_gov_au.py
@@ -1,90 +1,45 @@
-import logging
-from datetime import datetime
-
 import requests
-from bs4 import BeautifulSoup
+import json
+
+from datetime import datetime
 from waste_collection_schedule import Collection  # type: ignore[attr-defined]
 
 TITLE = "Wyndham City Council, Melbourne"
 DESCRIPTION = "Source for Wyndham City Council rubbish collection."
 URL = "https://wyndham.vic.gov.au"
+
 TEST_CASES = {
-    "Truganina South Primary School": {
-        "street_address": "3-19 Parkvista Drive TRUGANINA 3029"
-    },
-    "Westbourne Grammar School": {"street_address": "300 Sayers Road TRUGANINA 3029"},
-    "Werribee Mercy Hospital": {
-        "street_address": "300-310 Princes Highway WERRIBEE 3030"
-    },
-    "Wyndham Park Primary School": {
-        "street_address": "59-77 Kookaburra Avenue WERRIBEE 3030"
-    },
+    "Coord_001": {"lat": "-37.9030792", "lng": "144.6581664"},
 }
-
-API_URL = "https://digital.wyndham.vic.gov.au/myWyndham/"
+HEADERS = {
+    "user-agent": "Mozilla/5.0",
+}
 ICON_MAP = {
-    "Green Waste": "mdi:leaf",
-    "Garbage": "mdi:trash-can-outline",
-    "Recycling": "mdi:recycle",
+    "Green_Waste": "mdi:leaf",
+    "Waste": "mdi:trash-can-outline",
+    "Recycle": "mdi:recycle",
 }
-
-_LOGGER = logging.getLogger(__name__)
-
 
 class Source:
-    def __init__(self, street_address):
-        self._street_address = street_address
+    def __init__(self, lat, lon):
+        self._lat = lat
+        self._lng = lon
 
     def fetch(self):
-        session = requests.Session()
-        response = session.get(API_URL)
-        response.raise_for_status()
-        response = session.get(
-            "https://digital.wyndham.vic.gov.au/myWyndham/ajax/address-search-suggestions.asp?",
-            params=dict(ASEARCH=self._street_address),
-        )
-        response.raise_for_status()
-        html = response.content
-        property_address = BeautifulSoup(html, "html.parser").find("li").get_text()
-        _LOGGER.debug("Fetched Property Address: %s", property_address)
-        if (
-            property_address == "No match found."
-            or property_address.upper() != self._street_address.upper()
-        ):
-            raise Exception(
-                f"Address search for '{self._street_address}' returned no results. Check your address on "
-                f"https://digital.wyndham.vic.gov.au/myWyndham/ "
-            )
 
-        property_number = BeautifulSoup(html, "html.parser").find("span").get_text()
-        _LOGGER.debug("Fetched Property Number: %s", property_number)
-        response = session.get(
-            "https://digital.wyndham.vic.gov.au/myWyndham/init-map-data.asp",
-            params=dict(
-                propnum=property_number, radius="1000", mapfeatures="23,37,22,33,35"
-            ),
-        )
-        response.raise_for_status()
-        wasteApiResult = response.content
-        soup = BeautifulSoup(wasteApiResult, "html.parser")
+        s = requests.Session()
+        r = s.get(f"https://digital.wyndham.vic.gov.au/mywyndham/JSON/show_closest_feed.php?lat={self._lat}&lng={self._lng}", headers=HEADERS)
+        json_data = json.loads(r.text)["waste_collection_information"]
+
         entries = []
 
-        for article in soup.findAll("div", {"class": "waste"}):
-            if article.get_text().startswith("Next"):
-                waste_type = (
-                    article.get_text()
-                    .strip()
-                    .split(":")[0][5:]
-                    .replace(" Collection", "")
+        for bin,time in json_data.items():
+            entries.append(
+                Collection(
+                    date=datetime.strptime(time, "%A, %d %B %Y").date(),
+                    t=bin,
+                    icon=ICON_MAP.get(bin),
                 )
-                _LOGGER.debug("Waste Type: %s", waste_type)
-                icon = ICON_MAP.get(waste_type)
-                _LOGGER.debug("Icon: %s", icon)
-                next_pickup_date = datetime.strptime(
-                    article.get_text().split(":")[1].strip(), "%A, %d %B %Y"
-                ).date()
-                _LOGGER.debug("Next Pickup Date: %s", next_pickup_date)
-                entries.append(
-                    Collection(date=next_pickup_date, t=waste_type, icon=icon)
-                )
+            )
+
         return entries
